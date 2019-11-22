83f73d98a940ba05b0634d9ba8375cf96f9be199
J.Z <yiz132@pitt.edu>
2019-11-22 14:21:36 -0500

    Backend_Start

---
+++ demo/src/test/java/Team_Random/demo/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package src.test.java.Team_Random.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
