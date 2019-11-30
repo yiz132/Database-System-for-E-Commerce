58c846b9e3ad92f6ef648e6c25974faa874e8333
Xiya Liu <xil230@pitt.edu>
2019-11-30 18:55:26 -0500

    Sign in & Sort

--- DBProject/src/main/web/SignIn.js
+++ DBProject/src/main/web/SignIn.js
@@ -2,14 +2,59 @@ function SignUp_jump() {
     window.location.href = "SignUp.html"
 }
 
+$(function(){
+    $("input[type='radio']").click(function(){
+        $("input[type='radio'][name='"+$(this).attr('name')+"']").parent().removeClass("checked")
+        $(this).parent().addClass("checked");
+    });
+});
+
+var jumpURL = '';
+function Detail() {
+    var radios = document.getElementsByName('Character');
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            var role = radios[i].value;
+            break;
+        }
+    }
+    if(role == 'HomeCustomer'){
+        jumpURL = '';
+
+    }
+    else if(role == 'BusinessCustomer'){
+        jumpURL = '';
+
+
+    }
+    else if(role == 'SalesPerson'){
+        jumpURL = '';
+
+
+    }
+    else if(role == 'StoreManager'){
+        jumpURL = '';
+
+    }
+    else if(role == 'RegionManager'){
+        jumpURL = '';
+
+    }
+    else{ // infinite
+        //document.getElementsByName('Character').style.animation= "fade 600ms infinite";
+        //$("#Character1").css({"-webkit-animation":"twinkling 1s infinite ease-in-out"});
+
+
+    }
+}
+
 function SignIn(){
     var name = $('#UserName-register').val();
     //alert(name);
     var password = $('#FirstPassword-register').val();
-    var Url="http://localhost:8080/dbproject/home/signin";
     $.ajax({
         type: "post",
-        url: Url,
+        url: jumpURL,
         data: {
             name: name,
             password: password,
@@ -17,20 +62,15 @@ function SignIn(){
         },
         dataType: "json",
         success: function(msg) {
-            var data=eval(msg);
-            //alert(data);
-            var str = JSON.stringify(data);
-            //alert(str);
-            var obj = JSON.parse(str);
-            alert(obj.name);
+            // var data=eval(msg);
+            // //alert(data);
+            // var str = JSON.stringify(data);
+            // //alert(str);
+            // var obj = JSON.parse(str);
+            // alert(obj.name);
 
+            //传回role给前端
 
-            //alert("success!");
-            // var data = '';
-            // if (msg != '') {
-            //     //data = eval("(" + msg + ")");
-            //     alert(msg);
-            // }
         },
         error: function (request, status, error) {
             alert(request.responseText);
---
+++ DBProject/src/main/web/StoreManagerIndex.js
@@ -0,0 +1,40 @@
+function SearchBusiness() {
+
+}
+
+function SearchPress() {
+
+    if(event.keyCode==13)
+    {
+        SearchBusiness();
+    }
+
+}
+
+function SortChange(){
+    //alert(document.getElementById('Sort').value);
+    var sort = document.getElementById('Sort').value;
+    var Url="";
+    $.ajax({
+        type: "post",
+        url: Url,
+        data: {
+            sort: sort,
+
+        },
+        dataType: "json",
+        success: function(msg) {
+            // var data=eval(msg);
+            // //alert(data);
+            // var str = JSON.stringify(data);
+            // //alert(str);
+            // var obj = JSON.parse(str);
+            // alert(obj.name);
+
+        },
+        error: function (request, status, error) {
+            //alert(request.responseText);
+
+        }
+    });
+}
