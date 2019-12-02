22f1f9f3713b5c24ff8d3b769f42ee9d1b470532
J.Z <yiz132@pitt.edu>
2019-12-02 18:45:37 -0500

    region names

--- DBProject/src/main/java/team_random/DBProject/repository/RegionRepository.java
+++ DBProject/src/main/java/team_random/DBProject/repository/RegionRepository.java
@@ -1,9 +1,14 @@
 package team_random.DBProject.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import team_random.DBProject.model.Region;
+
+import java.util.List;
 
 public interface RegionRepository extends CrudRepository<Region, Integer> {
     Region findByName(String name);
     Region findById(int id);
+    @Query(value = "SELECT DISTINCT name FROM regions ", nativeQuery = true)
+    List<String> findAllNames();
 }
