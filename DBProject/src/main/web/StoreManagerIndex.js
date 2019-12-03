bba99abe534c99853d5d27ea8286986ed79a8f2e
Xiya Liu <xil230@pitt.edu>
2019-12-03 01:54:51 -0500

    Sign In JUMP

--- DBProject/src/main/web/StoreManagerIndex.js
+++ DBProject/src/main/web/StoreManagerIndex.js
@@ -24,28 +24,30 @@ function  ProductOnload() {
         type: "post",
         url: Url,
         data: {
-            id: obj.value,
+            store_manager_id: obj.value,
         },
         dataType: "json",
         success: function(msg) {
             var data=eval(msg);
             var str = JSON.stringify(data);
             var product = JSON.parse(str);
-            var picture = product.picture;
-            var name = product.name;
-            var price = product.price;
-            var sales = product.sales;
-            var profit = parseInt(price) * parseInt(sales);
-            var inventory = product.inventory;
-            document.getElementById('ListTable').innerText =document.getElementById('ListTable').innerText + "\n" +
-                "    <tr>\n" +
-                "        <td style=\"width: 90px;background-image: url("+picture+"); background-size: cover\"></td>\n" +
-                "        <td>"+name+"</td>\n" +
-                "        <td>"+price+"</td>\n" +
-                "        <td>"+sales+"</td>\n" +
-                "        <td>"+profit+"</td>\n" +
-                "        <td>"+inventory+"</td>\n" +
-                "    </tr>";
+            for( var i = 0; i<product.length; i++) {
+                var picture = product[i].picture;
+                var name = product[i].name;
+                var price = product[i].price;
+                var sales = product[i].sales;
+                var profit = parseInt(price) * parseInt(sales);
+                var inventory = product[i].inventory;
+                document.getElementById('ListTable').innerHTML = document.getElementById('ListTable').innerHTML + "\n" +
+                    "    <tr>\n" +
+                    "        <td style=\"width: 90px;background-image: url(" + picture + "); background-size: cover\"></td>\n" +
+                    "        <td>" + name + "</td>\n" +
+                    "        <td>" + price + "</td>\n" +
+                    "        <td>" + sales + "</td>\n" +
+                    "        <td>" + profit + "</td>\n" +
+                    "        <td>" + inventory + "</td>\n" +
+                    "    </tr>";
+            }
 
         },
         error: function (request, status, error) {
