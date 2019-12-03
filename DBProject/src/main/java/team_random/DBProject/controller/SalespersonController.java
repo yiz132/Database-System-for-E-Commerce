c0cbbfce7fb2616fa675cb9f13d4f7142f67577b
J.Z <yiz132@pitt.edu>
2019-12-02 21:05:24 -0500

    products by salesperson id

--- DBProject/src/main/java/team_random/DBProject/controller/SalespersonController.java
+++ DBProject/src/main/java/team_random/DBProject/controller/SalespersonController.java
@@ -97,6 +97,12 @@ public class SalespersonController {
         return product;
     }
 
+    @PostMapping(path = "/findbypid")
+    public @ResponseBody
+    Product findByPid(@RequestParam int pid){
+        return productService.findById(pid);
+    }
+
 
     //list transactions based on region id
     @PostMapping(path = "/regionmanager/showTransactions")
