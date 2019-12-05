7cd614972761c2adc985b578a11c508ab04cba79
Xiya Liu <xil230@pitt.edu>
2019-12-04 19:26:27 -0500

    Sign In JUMP

--- DBProject/src/main/web/RegionManagerIndex.js
+++ DBProject/src/main/web/RegionManagerIndex.js
@@ -4,7 +4,18 @@ function SearchBusiness() {
     if(sort == 'Stores'){
         window.location.href = "SortByStore.html?value=" + obj.value;
     }
-    else {
+    else if(sort == 'SalesHighToLow' || sort == 'SalesLowToHigh'){
+        document.getElementById('CanvasDiv').innerHTML = "";
+        document.getElementById('CanvasDiv').innerHTML = "<canvas id=\"a_canvas\" class=\"Canvas\" width=\"1000\" height=\"700\"></canvas>";
+        sales();
+
+    }
+    else if(sort == 'ProfitHighToLow' || sort == 'ProfitLowToHigh'){
+        document.getElementById('CanvasDiv').innerHTML = "";
+        document.getElementById('CanvasDiv').innerHTML = "<canvas id=\"a_canvas\" class=\"Canvas\" width=\"1000\" height=\"700\"></canvas>";
+        profits();
+
+    }
         document.getElementById('ListTable').innerHTML = "<tr style=\"height: 10px;\">\n" +
             "        <th style=\"height: 10px; border: 1px  lightpink; border-style: none none solid none;\">Region</th>\n" +
             "        <th style=\"height: 10px;  border: 1px  lightpink; border-style: none none solid none;\">Sales</th>\n" +
@@ -41,7 +52,7 @@ function SearchBusiness() {
 
             }
         });
-    }
+
 }
 
 function SearchPress() {
@@ -145,3 +156,251 @@ function onloadManager() {
 //         }
 //     });
 // }
+function sales(){
+
+
+        var Url = "http://localhost:8080/dbproject/regionmanager/showtrans/allregions";
+        $.ajax({
+            type: "get",
+            url: Url,
+            data: {},
+            dataType: "json",
+            success: function (msg) {
+                var data = eval(msg);
+                var str = JSON.stringify(data);
+                var manager = JSON.parse(str);
+
+                var regions = new Array()
+                var sales = new Array()
+                var profits = new Array()
+
+                for (var i = 0; i < manager.length; i++) {
+                    regions[i] = manager[i].name;
+                    sales[i] = manager[i].total_sales;
+                    profits[i] = manager[i].total_profits;
+                }
+                var data = sales;
+                var xinforma = regions;
+                var profit = profits;
+
+                var a_canvas = document.getElementById('a_canvas');
+                var context = a_canvas.getContext("2d");
+
+
+                // 绘制背景
+                var gradient = context.createLinearGradient(0, 0, 0, 300);
+
+
+                // gradient.addColorStop(0,"#e0e0e0");
+                //gradient.addColorStop(1,"#ffffff");
+
+
+                context.fillStyle = gradient;
+
+                context.fillRect(0, 0, a_canvas.width, a_canvas.height);
+
+                var realheight = a_canvas.height - 15;
+                var realwidth = a_canvas.width - 40;
+                // 描绘边框
+                var grid_cols = data.length + 1;
+                var grid_rows = 4;
+                var cell_height = realheight / grid_rows;
+                var cell_width = realwidth / grid_cols;
+                context.lineWidth = 1;
+                context.strokeStyle = "#a0a0a0";
+
+                // 结束边框描绘
+                context.beginPath();
+                // 准备画横线
+                /*for(var row = 1; row <= grid_rows; row++){
+                  var y = row * cell_height;
+                  context.moveTo(0,y);
+                  context.lineTo(a_canvas.width, y);
+                }*/
+
+                //划横线
+                context.moveTo(0, realheight);
+                context.lineTo(realwidth, realheight);
+
+
+                //画竖线
+                context.moveTo(0, 20);
+                context.lineTo(0, realheight);
+                context.lineWidth = 1;
+                context.strokeStyle = "black";
+                context.stroke();
+
+
+                var max_v = 0;
+
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i] > max_v) {
+                        max_v = data[i]
+                    }
+                    ;
+                }
+                max_v = max_v * 1.1;
+                // 将数据换算为坐标
+                var points = [];
+                for (var i = 0; i < data.length; i++) {
+                    var v = data[i];
+                    var px = cell_width * (i + 1);
+                    var py = realheight - realheight * (v / max_v);
+                    //alert(py);
+                    points.push({"x": px, "y": py});
+                }
+
+                //绘制坐标图形
+                for (var i in points) {
+                    var p = points[i];
+                    context.beginPath();
+                    context.fillStyle = "lightgreen";
+                    context.fillRect(p.x, p.y, 15, realheight - p.y);
+
+                    context.fill();
+                }
+
+                //添加文字
+                for (var i in points) {
+                    var p = points[i];
+                    context.beginPath();
+                    context.fillStyle = "black";
+                    context.fillText(data[i], p.x + 1, p.y - 15);
+                    context.fillText(xinforma[i], p.x + 1, realheight + 12);
+                    context.fillText('Regions', realwidth, realheight + 12);
+                    context.fillText('Sales', 0, 10);
+                }
+
+            },
+            error: function (request, status, error) {
+                //alert(request.responseText);
+
+            }
+        });
+
+    }
+
+
+function profits(){
+
+
+    var Url = "http://localhost:8080/dbproject/regionmanager/showtrans/allregions";
+    $.ajax({
+        type: "get",
+        url: Url,
+        data: {},
+        dataType: "json",
+        success: function (msg) {
+            var data = eval(msg);
+            var str = JSON.stringify(data);
+            var manager = JSON.parse(str);
+
+            var regions = new Array()
+            var sales = new Array()
+            var profits = new Array()
+
+            for (var i = 0; i < manager.length; i++) {
+                regions[i] = manager[i].name;
+                sales[i] = manager[i].total_sales;
+                profits[i] = manager[i].total_profits;
+            }
+            var data = profits;
+            var xinforma = regions;
+            //var profit = profits;
+
+            var a_canvas = document.getElementById('a_canvas');
+            var context = a_canvas.getContext("2d");
+
+
+            // 绘制背景
+            var gradient = context.createLinearGradient(0, 0, 0, 300);
+
+
+            // gradient.addColorStop(0,"#e0e0e0");
+            //gradient.addColorStop(1,"#ffffff");
+
+
+            context.fillStyle = gradient;
+
+            context.fillRect(0, 0, a_canvas.width, a_canvas.height);
+
+            var realheight = a_canvas.height - 15;
+            var realwidth = a_canvas.width - 40;
+            // 描绘边框
+            var grid_cols = data.length + 1;
+            var grid_rows = 4;
+            var cell_height = realheight / grid_rows;
+            var cell_width = realwidth / grid_cols;
+            context.lineWidth = 1;
+            context.strokeStyle = "#a0a0a0";
+
+            // 结束边框描绘
+            context.beginPath();
+            // 准备画横线
+            /*for(var row = 1; row <= grid_rows; row++){
+              var y = row * cell_height;
+              context.moveTo(0,y);
+              context.lineTo(a_canvas.width, y);
+            }*/
+
+            //划横线
+            context.moveTo(0, realheight);
+            context.lineTo(realwidth, realheight);
+
+
+            //画竖线
+            context.moveTo(0, 20);
+            context.lineTo(0, realheight);
+            context.lineWidth = 1;
+            context.strokeStyle = "black";
+            context.stroke();
+
+
+            var max_v = 0;
+
+            for (var i = 0; i < data.length; i++) {
+                if (data[i] > max_v) {
+                    max_v = data[i]
+                }
+                ;
+            }
+            max_v = max_v * 1.1;
+            // 将数据换算为坐标
+            var points = [];
+            for (var i = 0; i < data.length; i++) {
+                var v = data[i];
+                var px = cell_width * (i + 1);
+                var py = realheight - realheight * (v / max_v);
+                //alert(py);
+                points.push({"x": px, "y": py});
+            }
+
+            //绘制坐标图形
+            for (var i in points) {
+                var p = points[i];
+                context.beginPath();
+                context.fillStyle = "lightblue";
+                context.fillRect(p.x, p.y, 15, realheight - p.y);
+
+                context.fill();
+            }
+
+            //添加文字
+            for (var i in points) {
+                var p = points[i];
+                context.beginPath();
+                context.fillStyle = "black";
+                context.fillText(data[i], p.x + 1, p.y - 15);
+                context.fillText(xinforma[i], p.x + 1, realheight + 12);
+                context.fillText('Regions', realwidth, realheight + 12);
+                context.fillText('Profits', 0, 10);
+            }
+
+        },
+        error: function (request, status, error) {
+            //alert(request.responseText);
+
+        }
+    });
+
+}
